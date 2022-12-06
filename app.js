1ee5824d1c36dd1233f29d7c380572d80eaf9a83
Leila <Leeigregorio@gmail.com>
2022-12-06 14:32:42 -0300

    update 1.0

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static("public"));
 app.set("view engine","ejs");
 app.set("views", "./src/views");
 
-app.get("/" , (req,res) => {
+app.get("/", (req,res) => {
     res.send("CRUD");
 }); 
 app.use("/posts",require("./src/routes/posts"));
 
-const PORT = 5000;
-app.listen( PORT, () => console.log("https://localhost:${PORT}"));
+const PORT = 8080;
+app.listen(PORT,()=>console.log('http://localhost:${PORT}'));
